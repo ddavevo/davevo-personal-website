2a093220594876a30d061df88c4919d8cc1329a0
ddavevo <ddavepersona@gmail.com>
2024-06-30 19:02:59 -0700

    Fixed hyperlinks

---
+++ javascript/case-study-links.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#aromacovery").click(function() {
+        window.location.href = 'case-studies/aromacovery.html';
+    });
+});
+
+$(document).ready(function() {
+    $("#sevenatenine").click(function() {
+        window.location.href = 'case-studies/7ate9.html';
+    });
+});
