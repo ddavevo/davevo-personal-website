db496b70c3a252d32f98484b02e110e045257aa3
ddavevo <ddavepersona@gmail.com>
2026-02-14 23:26:38 -0800

    removing github static in favor of serverside

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://ddavevo.github.io',
-  base: '/davevo-personal-website/',
-});
+export default defineConfig({});
